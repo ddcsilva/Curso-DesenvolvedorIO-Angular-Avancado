38abac8fb05213a63ff4685928205396425d0720
Danilo Silva <danilo.silva@msn.com>
2023-02-13 07:19:08 -0300

    Cadastro de novo usuário

--- Projeto-Final/front-end/src/app/conta/conta.module.ts
+++ Projeto-Final/front-end/src/app/conta/conta.module.ts
@@ -1,14 +1,18 @@
-import { ContaAppComponent } from './conta.app.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { CustomFormsModule } from 'ng2-validation';
+
 import { ContaRoutingModule } from './conta.route';
+
+import { ContaService } from './services/conta.service';
 
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+import { ContaAppComponent } from './conta.app.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { LoginComponent } from './login/login.component';
     ContaRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CustomFormsModule
+  ],
+  providers: [
+    ContaService
   ]
 })
 export class ContaModule { }
